97ff3a96f2542e9164c018c3cda8370d03f156b4
trentglobal-sg <paul.chor@trentglobal.edu.sg>
2026-05-05 07:25:16 +0000

    Added form

--- 02-forms/script.js
+++ 02-forms/script.js
@@ -11,7 +11,30 @@ document.querySelector("#submitBtn")
             const selectedAgeGroupEl = document.querySelector(".age-group:checked");
             const ageGroup = selectedAgeGroupEl.value;
 
+            // when we want to convert one array to another array, but slightly different,
+            // it's known mapping
+            
+            // get all the selected checkboxes
+            const selectedDestinationsEl = document.querySelectorAll(".destinations:checked");
+
+            // we want to get a new array from the selected checkboxes, but the array will contain
+            // the value of each of the selected checkboxes
+            let selectedDestinations = [];
+            for (let checkbox of selectedDestinationsEl) {
+                selectedDestinations.push(checkbox.value);
+            }
+
+            // get the counrty of the user
+            const country = document.querySelector("#country").value;
+
+            // alternatively
+            // const countryEl = document.querySelector("#country");
+            // const country = countryEl.value
+
+
             console.log("User name =", userName);
             console.log("User Email =", userEmail);
             console.log("Age Group =", ageGroup);
+            console.log("Selected destinations =", selectedDestinations);
+            console.log("selected country =", country)
         })
