5587b55207038fa6301d1c9ed25aace668520674
trentglobal-sg <paul.chor@trentglobal.edu.sg>
2026-05-07 07:05:46 +0000

    Added solution for q2

---
+++ 05-solutions/02-json/index.js
@@ -0,0 +1,17 @@
+const url = "https://raw.githubusercontent.com/kunxin-chor/data-files-and-stuff/master/json-simple.json";
+
+async function loadData() {
+    const response = await axios.get(url);
+    return response.data;
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+    const userData = await loadData();
+    const outputUl = document.querySelector("#output");
+    outputUl.innerHTML = `
+        <li>Name: ${userData.Name}</li>  
+        <li>Mobile Number: ${userData.Mobile}</li>
+        <li>Current Address: ${userData.Address["current Address"]}</li>
+        <li>Permanent Address: ${userData.Address["Permanent address"]}</li>
+    `
+});
